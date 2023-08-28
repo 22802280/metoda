8ddaabf8c3468cf50b2ac5d5811349312fdca50a
22802280 <104138799+22802280@users.noreply.github.com>
2023-08-29 01:07:36 +0300

    final

--- main.js
+++ main.js
@@ -3,17 +3,15 @@ document.querySelector(".mobile-button").addEventListener("click", function(even
     document.querySelector(".mobile-menu").classList.toggle("hide");
 });
 
-
 document.querySelector(".fixed-menu").addEventListener('click', function(event){
     let clickTarget = event.target;
     let activeBtn = document.querySelector('.active');
-    if (clickTarget.classList.contains('nav-link') && !clickTarget.classList.contains('active')){
+    if (clickTarget.classList.contains('nav-link') ){
         clickTarget.classList.add('active');
         activeBtn.classList.remove('active')
         let mobile = document.querySelector('.mobile-menu');
         if (!mobile.classList.contains('hide')){
             mobile.classList.add('hide');
-            
         }
     }
-})
+});
